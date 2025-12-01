5260c81fdfef6e37cc66004cede7e938c7d0bd03
Ayush Kumar Singh <ayushkumarsingh@Ayushs-MacBook-Air.local>
2025-12-01 10:51:06 +0530

    vercel

--- JS/NodeJS/backtick.js
+++
@@ -1,5 +0,0 @@
-let fname="Ayush";
-let age=21;
-
-// console.log("My name is Ayush and I am " + age + " years old.");
-console.log(`My name is ${fname} and I am ${age} years old.`);
--- JS/NodeJS/map_reduce_filter.js
+++
@@ -1,8 +0,0 @@
-let ans=arr.map((element)=>{
-    return element*3;
-    if(element%2===0){
-        return element*2;
-    }   
-});
-
-console.log(ans);
